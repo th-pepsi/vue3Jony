5e4f0aa62f567d3f3d8ac9c29f3d6678bf71dd95
Jony.Tao <Jony.Tao@batechworks.com>
2022-04-19 15:10:03 +0800

    feat:add dependences

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 defineProps<{ msg: string }>()
-
 const count = ref(0)
+function setValue() {
+  count.value++
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div>{{ count }}</div>
+    <button class="absolute bottom-5 right-0 left-0 text-center op30 fw300" @click="setValue">增加</button>
+  </div>
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Unocss({ /* options */ }),
+    AutoImport({
+      imports: ['vue', '@vueuse/core']
+     })
+  ]
 })
